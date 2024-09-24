9fa9f61fc131d43553d1de8c5de1ab71ea05d258
Truman J <tjarmusch878@gmail.com>
2024-09-24 18:10:44 +0000

    Obvious

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World!!!!")
+print("Welcome to the Elite 101 Prework Chatbot!")
+name = input('Please input your name here:')
+print('Hello,', name)
+print('How may I assist you today?')
+print('1. Placeholder Selection')
+print('2. Placeholder Selection')
+print('3. PLaceholder Selection')
+print('4. End program')
+num = int(input('Please select a numerical option from the list above.'))
+if num == 1:
+    print('Stop it. Get some help.')
+if num == 2:
+    print('Cool story, bro.')
+if num == 3:
+    print('Error 404')
+if num == 4:
+    print("It's about time you left. Finally, peace and quiet.")
